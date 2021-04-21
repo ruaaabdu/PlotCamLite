9304f3d5c09774db39f2232b6ccb98ae8a576650
ruaaabdu <47121177+ruaaabdu@users.noreply.github.com>
2021-04-21 11:38:01 -0400

    Add files via upload

---
+++ components/metadata.py
@@ -0,0 +1,100 @@
+####################################################
+# metadata.py v0.2
+####################################################
+"""
+This code handles the metadata functions
+@author Ruaa Abdulmajeed
+@date February 23rd, 2020
+"""
+import json
+import os
+from datetime import datetime
+
+class Metadata:
+    """
+    An instance of this class creates/reads a file in the given directory to store a metadata file. This class also contains
+    functions to manipulate the data.
+    """
+
+    def __init__(self, metadata_fpath):
+        """Constructor for Metadata class.
+
+        Args:
+            metadata_fpath (str): File path of metadata file
+        """
+
+        self.filepath = metadata_fpath
+        self.new_data = False
+        self.load_data()
+
+    def load_data(self):
+        """
+        Prepare metadata information from the filepath.
+        If file exists, the existing information is stored, else creates a new file.
+        """
+        
+        if not os.path.exists(self.filepath) or os.path.getsize(self.filepath,) == 0:
+            with open(self.filepath, "w") as fp:
+                self.metadata = []
+        else:
+            with open(self.filepath, "r") as f:
+                self.metadata = json.load(f)
+    
+    # def load_existing_experiment_metadata(self, existing_filepath):
+
+
+    def add_entry(self, number, time, xpos, ypos, air, light, lat, lon, batt, zero, plant_height):
+        """Appends plot data to metadata.
+
+        Args:
+            number (str): The plot number, padded by 0s on the left so that its always 3 digits
+            time (str): The time the image was taken at
+            xpos (int): The x coordinate
+            ypos (int): The y coordinate
+            air (int): The air
+            light (int): The light
+            lat (int): The lattitude where the picture was taken
+            lon (int): The longitude where the picture was taken
+            batt (int): The battery level
+            zero (int): The zeroing value
+            plantheight (int): The average plant height
+        """
+        image_data = {
+            "number": number,
+            "time": time,
+            "xpos": xpos,
+            "ypos": ypos,
+            "air": air,
+            "light": light,
+            "lattitude": lat,
+            "longitude": lon,
+            "battery": batt,
+            "zeroing_value": zero,
+            "plant_height": plant_height,
+        }
+        self.metadata.append(image_data)
+        self.new_data = True
+
+    def get_last_index(self):
+        """Returns the plot number of the last plot captures
+
+        Returns:
+            int: The plot number of the last element
+        """
+        if len(self.metadata) == 0:  # file is empty
+            return -1
+
+        return int(self.metadata[-1]["number"])
+
+    def save(self):
+        """
+        Save metadata to file.
+        """    
+        if not self.new_data:
+            return
+        
+        with open(self.filepath, 'w') as f:
+            # Saves metadata entry and formatting using indent=4
+            json.dump(self.metadata, f, indent=4)
+        
+        self.new_data = False
---
+++ components/target.py
@@ -0,0 +1,66 @@
+####################################################
+# target.py v0.1
+####################################################
+"""
+This code handles the target functions
+@author Ruaa Abdulmajeed
+@date February 23rd, 2021
+"""
+
+import sys
+from PyQt5.QtCore import Qt, QRect, QPointF
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QPixmap, QPainter, QPen
+
+class Target(QWidget):
+    def __init__(self):
+        super().__init__()
+        self._pixmap = QPixmap()
+        self._coordinate = QPointF()
+
+    @property
+    def pixmap(self):
+        return self._pixmap
+
+    @pixmap.setter
+    def pixmap(self, pixmap):
+        self._pixmap = pixmap.copy()
+        self.update()
+        size = self.pixmap.size()
+        if size.isValid():
+            self.resize(size)
+        else:
+            self.resize(300, 300)
+
+    @property
+    def coordinate(self):
+        return self._coordinate
+
+    @coordinate.setter
+    def coordinate(self, point):
+        self._coordinate = point
+        self.update()
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.drawPixmap(self.rect(), self.pixmap)
+        r = QRect(-1, -1, 2, 2)
+        painter.setWindow(r)
+        pen = QPen(Qt.red, 0.5, Qt.DotLine, Qt.RoundCap)
+        painter.setPen(pen)
+        painter.drawPoint(self.coordinate)
+        #print("curent coordinate = " + str(self.coordinate))
+
+
+# TODO delete main, add comments
+if __name__ == "__main__":
+    """For testing purposes"""
+    app = QApplication(sys.argv)
+    ex = Target()
+    ex.pixmap = QPixmap(r"C:\Users\yozoh\Documents\random code\plotcam-lite\resources\icons\target_png_300.png")
+    ex.coordinate = QPointF(0, 0)
+    ex.coordinate = QPointF(0.5, 0.5)
+    ex.coordinate = QPointF(0.75, 0.75)
+    ex.coordinate = QPointF(1, 1)
+    ex.show()
+    sys.exit(app.exec_())
